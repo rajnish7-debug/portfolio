b45fc2dc4ff71240db3fc1a2fe7eca1136bbfdac
rajnish7-debug <120913747+rajnish7-debug@users.noreply.github.com>
2023-02-19 22:53:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*--=========================typing annimation=============*/
+var typed = new Typed(".typing" ,{
+    strings:["","Web Designer","web Developer","Graphic Designer","Stratregist"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
+
+
+
+
